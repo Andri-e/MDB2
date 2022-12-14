95ee70cb555f2dddd775973812101563f77def7f
JAco9307 <skrivtiljacob@hotmail.com>
2022-12-14 09:15:34 +0100

    Added viewpoint and labelled all items

--- Visualizer.py
+++ Visualizer.py
@@ -17,7 +17,7 @@ class Window(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.geometry('600x400')
+        self.geometry('600x500')
         self.title('Toplevel Window')
 
         self.canvas = Canvas(self, background="white")
@@ -27,6 +27,10 @@ class Window(tk.Toplevel):
         self.laser_img = self.laser_img.resize((90, 15), Image.ANTIALIAS)
         self.laser_img = ImageTk.PhotoImage(self.laser_img)
 
+        self.cam_img = Image.open("Images/cam.png")
+        self.cam_img = self.cam_img.resize((40, 40), Image.ANTIALIAS)
+        self.cam_img = ImageTk.PhotoImage(self.cam_img)
+
         self.splitter_img = Image.open("Images/splitter.png")
         self.splitter_img = self.splitter_img.resize((90, 15), Image.ANTIALIAS)
         self.splitter_img = ImageTk.PhotoImage(self.splitter_img.rotate(45, expand=True))
@@ -35,6 +39,10 @@ class Window(tk.Toplevel):
         self.mirror_image = self.mirror_image.resize((90, 15), Image.ANTIALIAS)
         self.mirror_image1 = ImageTk.PhotoImage(self.mirror_image)
         self.mirror_image2 = ImageTk.PhotoImage(self.mirror_image.rotate(90, expand=True))
+
+        self.lens_img = Image.open("Images/concave_lens.png")
+        self.lens_img = self.lens_img.resize((90, 15), Image.ANTIALIAS)
+        self.lens_img = ImageTk.PhotoImage(self.lens_img)
 
         self.laser_canvas = self.canvas.create_image(grid_pos.x0,
                                                      grid_pos.y2+33,
@@ -54,12 +62,34 @@ class Window(tk.Toplevel):
                                                       anchor=NW,
                                                       image=self.mirror_image2)
 
+        self.lens_canvas = self.canvas.create_image(grid_pos.x1,
+                                                    grid_pos.y2+200,
+                                                    anchor=NW,
+                                                    image=self.lens_img)
+
+        self.cam_canvas = self.canvas.create_image(grid_pos.x1+25,
+                                                    grid_pos.y2+280,
+                                                    anchor=NW,
+                                                    image=self.cam_img)
+
         self.canvas.create_line(grid_pos.x1+45,
                                 grid_pos.y2+40,
                                 grid_pos.x1+45,
                                 grid_pos.y1+15,
                                 width=3,
                                 fill='red')
+
+        self.canvas.create_line(grid_pos.x1+45,
+                                grid_pos.y2+40,
+                                grid_pos.x1+45,
+                                grid_pos.y2+210,
+                                width=3,
+                                fill='red')
+
+        points = [grid_pos.x1+45, grid_pos.y2+190,
+                  grid_pos.x1+35, grid_pos.y2+280,
+                  grid_pos.x1+55, grid_pos.y2+280]
+        self.canvas.create_polygon(points, width=3, fill='red')
 
         self.laser1 = self.canvas.create_line(grid_pos.x1+45,
                                               grid_pos.y2+40,
@@ -75,7 +105,7 @@ class Window(tk.Toplevel):
                                 fill='red')
 
         self.scale_label = ttk.Label(self,text="Model not to scale")
-        self.scale_label.place(x=0, y=20)
+        self.scale_label.place(x=0, y=0)
 
         self.pointing_line = self.canvas.create_line(grid_pos.x2+1,
                                                      grid_pos.y2+89,
@@ -108,10 +138,15 @@ class Window(tk.Toplevel):
 
         self.canvas.create_text(grid_pos.x2+pos_scale*0.15, grid_pos.y2+95,  text='31 mm')
 
-        self.pointing_label = self.canvas.create_text(grid_pos.x2+22, grid_pos.y2+90+34,  text='20 mm')
+        self.pointing_label = self.canvas.create_text(grid_pos.x2+25, grid_pos.y2+90+34,  text='20.0 mm')
 
-
-
+        # Item labels
+        self.canvas.create_text(grid_pos.x1+20, grid_pos.y1-10,  text='Mirror')
+        self.canvas.create_text(grid_pos.x1, grid_pos.y2+20,  text='Beam splitter')
+        self.canvas.create_text(grid_pos.x1-10, grid_pos.y2+300,  text='Viewpoint')
+        self.canvas.create_text(grid_pos.x0+30, grid_pos.y2+18,  text='Laser')
+        self.canvas.create_text(grid_pos.x1, grid_pos.y2+190,  text='Concave lens')
+        self.mirror_label = self.canvas.create_text(grid_pos.x2+4, grid_pos.y2-7,  text='Mirror')
 
 
 def update_text(window,z1, z2):
@@ -136,7 +171,13 @@ def update_text(window,z1, z2):
     window.canvas.coords(window.pointing_label,
                          pos+25,
                          grid_pos.y2+90+34)
+
+    window.canvas.coords(window.mirror_label,
+                         pos+4, grid_pos.y2-7)
+
     window.canvas.itemconfigure(window.pointing_label, text=f"{z1*100:.1f} mm")
+
+
 
 
 def slider_update_event(window, z1, z2_updated):
